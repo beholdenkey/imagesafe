71b257aadccd5e7bd1925badc65d10ff77b58682
Justin Bailey <justthered63@gmail.com>
2024-12-11 20:22:52 -0500

    ci: add foundational configurations (#4)
    * chore: update .gitignore to include Taskfile and SBOM files
    
    * chore: add pre-commit configuration with various hooks
    
    * chore: add cspell configuration for spell checking
    
    * chore: add example.env for GitHub user and token configuration
    
    * chore: add 'imagesafe' to cspell configuration
    
    * chore: add Taskfile.yaml for build and task management
    
    * chore: add markdownlint and yamllint configurations for linting YAML files
    
    * chore: add Taskfile.yaml for managing Malcontent container image tasks
    
    * chore: update trufflehog to version 3.86.1 in pre-commit configuration
    
    * chore: add configuration files
    
    * chore: add issue templates for bug reports and feature requests
    
    * chore: add GitHub workflows for auto-assign, issue metrics, label sync, labeler, and security scanning
    
    * chore: add Taskfile configurations for pre-commit hooks and workstation setup
    
    * chore: add Brewfile with essential brew packages for workstation setup
    
    * chore: reset dagger
    
    Signed-off-by: Justin Bailey <justthered63@gmail.com>
    
    * ci: init dagger
    
    Signed-off-by: Justin Bailey <justthered63@gmail.com>
    
    ---------
    
    Signed-off-by: Justin Bailey <justthered63@gmail.com>

--- dagger/main.go
+++ dagger/main.go
@@ -1,4 +1,4 @@
-// A generated module for DaggerProvingGrounds functions
+// A generated module for Imagesafe functions
 //
 // This module has been generated via dagger init and serves as a reference to
 // basic module structure as you get started with Dagger.
@@ -15,69 +15,23 @@
 package main
 
 import (
-	"context"
-	"dagger/dagger-proving-grounds/internal/dagger"
-	"fmt"
+    "context"
+    "dagger/imagesafe/internal/dagger"
 )
 
-type DaggerProvingGrounds struct{}
+type Imagesafe struct{}
 
 // Returns a container that echoes whatever string argument is provided
-func (m *DaggerProvingGrounds) ContainerEcho(stringArg string) *dagger.Container {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
+func (m *Imagesafe) ContainerEcho(stringArg string) *dagger.Container {
+    return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
 }
 
 // Returns lines that match a pattern in the files of the provided Directory
-func (m *DaggerProvingGrounds) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
-	return dag.Container().
-		From("alpine:latest").
-		WithMountedDirectory("/mnt", directoryArg).
-		WithWorkdir("/mnt").
-		WithExec([]string{"grep", "-R", pattern, "."}).
-		Stdout(ctx)
-}
-
-// func (m *DaggerProvingGrounds) Build(ctx context.Context) error {
-// 	result := dag.Apko().Build(dag.CurrentModule().Source().File("wolfi-base.yaml"), "latest")
-// 	_, err := dag.Container().Import(result.File()).WithExec([]string{"cat", "/etc/apk/repositories"}).Sync(ctx)
-
-// 	return err
-// }
-
-// An Improved version of the Build function
-func (m *DaggerProvingGrounds) Build(ctx context.Context, src *dagger.File, tag string) error {
-	result := dag.Apko().Build(src, tag)
-	_, err := dag.Container().Import(result.File()).WithExec([]string{"cat", "/etc/apk/repositories"}).Sync(ctx)
-
-	return err
-}
-
-func (m *DaggerProvingGrounds) Wolfi(ctx context.Context) error {
-	const expected = "https://packages.wolfi.dev/os\n"
-
-	actual, err := dag.Apko().Wolfi().Container().WithExec([]string{"cat", "/etc/apk/repositories"}).Stdout(ctx)
-	if err != nil {
-		return err
-	}
-
-	if actual != expected {
-		return fmt.Errorf("expected %q, got %q", expected, actual)
-	}
-
-	return nil
-}
-
-func (m *DaggerProvingGrounds) Alpine(ctx context.Context) error {
-	const expected = "https://dl-cdn.alpinelinux.org/alpine/edge/main\n"
-
-	actual, err := dag.Apko().Alpine().Container().WithExec([]string{"cat", "/etc/apk/repositories"}).Stdout(ctx)
-	if err != nil {
-		return err
-	}
-
-	if actual != expected {
-		return fmt.Errorf("expected %q, got %q", expected, actual)
-	}
-
-	return nil
+func (m *Imagesafe) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
+    return dag.Container().
+        From("alpine:latest").
+        WithMountedDirectory("/mnt", directoryArg).
+        WithWorkdir("/mnt").
+        WithExec([]string{"grep", "-R", pattern, "."}).
+        Stdout(ctx)
 }
